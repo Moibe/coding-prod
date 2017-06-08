7385b449772f0ae7e60c7ca7b62a24037c7a3830
Walter Torres <walter@spraystudio.com.mx>
2017-06-08 18:10:23 -0500

    Menus mobile

--- src/AppBundle/Resources/public/template/js/custom-home.js
+++ src/AppBundle/Resources/public/template/js/custom-home.js
@@ -30,6 +30,39 @@ jQuery(function(){
 })
 
 jQuery(document).ready(function(){
+
+    
+
+        function runEffect() {
+      // get effect type from
+      var selectedEffect = "blind";
+ 
+      // Most effect types need no options passed by default
+      var options = {};
+      // some effects have required parameters
+      if ( selectedEffect === "scale" ) {
+        options = { percent: 50 };
+      } else if ( selectedEffect === "size" ) {
+        options = { to: { width: 200, height: 60 } };
+      }
+ 
+      // Run the effect
+      $( "#effect" ).toggle( selectedEffect, options, 500 );
+    };
+ 
+    // Set effect from select menu value
+    $( "#burguer" ).on( "click", function() {
+      runEffect();
+      
+    });
+
+    $(document).on('click', '.menu-mb-c li', function () {      
+      $( "#effect" ).toggle(500);    
+    });
+
+    // MENU
+
+
     if(!device.mobile() && !device.tablet()){
         jQuery('.header .header-content').tmStickUp({
             correctionSelector: jQuery('#wpadminbar')
