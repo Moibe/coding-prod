17aa6b43f517281bb5daca710c6db3b93015e9c3
Moibe <moi_estrello@hotmail.com>
2023-01-03 16:51:56 -0600

    Next via Visual

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -52,7 +52,7 @@ class DefaultController extends Controller {
         $redirect = $this->generateUrl('homepage');
 
         if (!$item->getFeatured()) {
-            $redirect = "https://www.geopositioningservices.com/uk/output-success.html";
+            $redirect = "https://www.geopositioningservices.com/{language}/output-success.html";
         }
 
         return $this->redirect($redirect);
